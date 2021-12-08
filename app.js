5da3af3af86ff87e4bc1105dd8e6d76e97871a4c
samuel <samuel.chowles@gmail.com>
2021-12-08 19:31:02 +0200

    render gamegrid

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hi!')
+const gameboard = (() => {
+    // create the game grid area
+    const render = () => {
+        const gameArea = document.querySelector('.gameArea');
+        let gameGrid = document.createElement('div');
+        gameGrid.id = ('game_grid');
+            for (let i = 0; i < 9 ; i++){
+            let block = document.createElement('div')
+            block.className = ('block')
+            gameGrid.appendChild(block);
+        }
+        gameArea.appendChild(gameGrid);
+}
+render();
+
+})();
+
