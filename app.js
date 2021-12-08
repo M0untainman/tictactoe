d4b246adefa18e7c2194340d6802a625a4101e3b
samuel <samuel.chowles@gmail.com>
2021-12-08 20:29:13 +0200

    placetokens array

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const gameboard = (() => {
     // create the game grid area
-    const render = () => {
+    const gameGridArray = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
+    const renderGrid = () => {
         const gameArea = document.querySelector('.gameArea');
         let gameGrid = document.createElement('div');
         gameGrid.id = ('game_grid');
-            for (let i = 0; i < 9 ; i++){
+        for (let i = 0; i < 9 ; i++){
             let block = document.createElement('div')
-            block.className = ('block')
+            block.className = ('blocks')
+            let blockNum = `block${i}`
+            block.classList.add(blockNum)
             gameGrid.appendChild(block);
         }
         gameArea.appendChild(gameGrid);
-}
-render();
+    }
+    
+    renderGrid();
+
+    const placeTokens = (gridArray) => {
+        for (let i = 0; i < 9; i++){
+            let position = document.querySelector(`.block${i}`);
+            position.innerHTML = gridArray[i];
+        }
+    }
+    placeTokens(gameGridArray);
 
 })();
 
