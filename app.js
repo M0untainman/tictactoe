23e031d2238e85a5d410281b7d5e5c290a6969da
samuel <samuel.chowles@gmail.com>
2021-12-09 16:49:30 +0200

    game winner can be found

--- app.js
+++ app.js
@@ -1,3 +1,64 @@
+// game control object
+const gameControl = (() => {
+    // dom declarations for player info
+    const player1token = document.getElementById('token1').value;
+    const player1name = document.getElementById('name1').value;
+    const player2token = document.getElementById('token2').value
+    const player2name = document.getElementById('name2').value;
+
+    // player creation
+    const playerFactory = (name, token) => {
+        return {name, token}
+    }
+
+    const player1 = playerFactory(player1name, player1token);
+    const player2 = playerFactory(player2name, player2token);
+    
+    // logic to control which players move it is
+    let activePlayer = player2;
+    
+    const switchPlayer = () => {
+        if (activePlayer === player1) {
+            activePlayer = player2;
+        }
+        else if (activePlayer === player2) {
+            activePlayer = player1;
+        }
+        return activePlayer.token
+        
+    };
+
+    // logic to find winner
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    const checkWin = () => {
+       winConditions.forEach((item) =>{
+           if (gameboard.gameGridArray[item[0]] === activePlayer.token && gameboard.gameGridArray[item[1]] === activePlayer.token && gameboard.gameGridArray[item[2]] === activePlayer.token){
+               console.log(`${activePlayer.name} wins`)
+           }
+       })  
+    }
+
+    return{
+        switchPlayer,
+        checkWin,
+        activePlayer
+
+    }
+ })();
+
+
+
+ // gameboard object
 const gameboard = (() => {
     // create the game grid area
     const gameGridArray = []
@@ -8,7 +69,9 @@ const gameboard = (() => {
         for (let i = 0; i < 9 ; i++){
             let block = document.createElement('div')
             block.className = ('blocks')
-            block.addEventListener('click', (e) => {takeTurn(e)})
+            block.addEventListener('click', (e) => {
+                takeTurn(e);
+            })
             let blockNum = i
             block.id = blockNum;
             gameGrid.appendChild(block);
@@ -26,48 +89,22 @@ const gameboard = (() => {
     const takeTurn = (e) => {
         let chosenBlock = e.srcElement.id;
         if (gameGridArray[chosenBlock] == null){
-            gameGridArray[chosenBlock] = switchPlayer();
+            gameGridArray[chosenBlock] = gameControl.switchPlayer();
+            gameControl.checkWin(gameGridArray);
             placeTokens(gameGridArray);
+            
         }
         else{
             alert('Not a legal move')
-        }
-        
+        }  
     }
     renderGrid();
-
+    return{
+        gameGridArray
+    }
 })();
 
-// const gameControl = (() => {
-    // dom declarations for player info
-    const player1token = document.getElementById('token1').value;
-    const player1name = document.getElementById('name1').value;
-    const player2token = document.getElementById('token2').value
-    const player2name = document.getElementById('name2').value;
 
-    const playerFactory = (name, token) => {
-        return {name, token}
-    }
-
-    const player1 = playerFactory(player1name, player1token);
-    const player2 = playerFactory(player2name, player2token);
-    
-    let activePlayer = player1;
-    const switchPlayer = () => {
-        
-        if (activePlayer == player1) {
-            activePlayer = player2;
-            return player1.token;
-        }
-        else if (activePlayer == player2) {
-            activePlayer = player1;
-            return player2.token;
-        }
-        
-    };
-
-    
-// })();
 
  
 
